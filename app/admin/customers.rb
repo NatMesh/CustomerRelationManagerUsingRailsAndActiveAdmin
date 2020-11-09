25e2096686ecb1ac597e734260c20900bbe3b33f
Nathanael <nathanael.meshesha@yahoo.com>
2020-11-09 03:22:43 -0600

    Added ActiveStorage and Started prep for Customer Attachments.

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Customer do
   #lets users modify elements of a Customer
-  permit_params :full_name, :phone_number, :email_address, :notes
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,4 @@
 class Customer < ApplicationRecord
   validates :full_name, :phone_number, :email_address, presence: true
+  has_one_attached :image
 end
